af91cb5dd325da64f8271f5f60e3f2faa4c2e6de
nely de faria <nelly.nf54@gmail.com>
2021-03-17 13:40:33 -0300

    Cria método create

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -16,12 +16,11 @@ class SeriesController extends Controller
             'Agents of SHIELD'
         ];
 
-        $html = "<ul>";
-        foreach ($series as $serie) {
-            $html .= "<li>$serie</li>";
-        }
-        $html .= "</ul>";
+        return view('series.index', compact('series'));
+    }
 
-        return $html;
+    public function create()
+    {
+        return view('series.create');
     }
 }
