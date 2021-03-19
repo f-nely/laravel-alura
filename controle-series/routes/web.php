3f209a10a1f1589c1b10254925cffd62b212bbaf
nely de faria <nelly.nf54@gmail.com>
2021-03-18 21:18:46 -0300

    Cria rota para remover séries

--- controle-series/routes/web.php
+++ controle-series/routes/web.php
@@ -16,7 +16,6 @@ Route::get('/', function () {
 });
 
 Route::get('/series', 'SeriesController@index');
-
 Route::get('/series/criar', 'SeriesController@create');
-
 Route::post('/series/criar', 'SeriesController@store');
+Route::delete('/series/{id}', 'SeriesController@destroy');
