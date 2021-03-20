43cffcbdad75fe1e90aca0f2ed32a44b5ab1f25c
nely de faria <nelly.nf54@gmail.com>
2021-03-20 12:08:35 -0300

    Além de utilização de transaction criação do método removerTemporas e removerEpisodios

--- controle-series/app/Services/RemovedorDeSerie.php
+++ controle-series/app/Services/RemovedorDeSerie.php
@@ -3,22 +3,46 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\DB;
 use App\{Serie, Temporada, Episodio};
 
 class RemovedorDeSerie
 {
     public function removerSerie(int $serieId): string
     {
-        $serie = Serie::find($serieId);
-        $nomeSerie = $serie->nome;
-        $serie->temporadas->each(function (Temporada $temporada) {
-            $temporada->episodios->each(function (Episodio $episodio) {
-                $episodio->delete();
-            });
-            $temporada->delete();
+        $nomeSerie = '';
+        DB::transaction(function () use ($serieId, &$nomeSerie){
+            $serie = Serie::find($serieId);
+            $nomeSerie = $serie->nome;
+            $this->removerTemporadas($serie);
+            $serie->delete();
         });
-        $serie->delete();
 
         return $nomeSerie;
+    }
+
+    /**
+     * @param $serie
+     * @throws \Exception
+     */
+    private function removerTemporadas(Serie $serie): void
+    {
+        $serie->temporadas->each(function (Temporada $temporada) {
+            $this->removerEpisodios($temporada);
+            $temporada->delete();
+        });
+
+    }
+
+    /**
+     * @param Temporada $temporada
+     * @throws \Exception
+     */
+    private function removerEpisodios(Temporada $temporada): void
+    {
+        $temporada->episodios->each(function (Episodio $episodio) {
+            $episodio->delete();
+        });
+
     }
 }
