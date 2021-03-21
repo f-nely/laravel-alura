0544f3d85a1b6983ff38d831131428cf02183316
nely de faria <nelly.nf54@gmail.com>
2021-03-21 19:38:19 -0300

    Adicionando navbar

--- controle-series/resources/views/layout.blade.php
+++ controle-series/resources/views/layout.blade.php
@@ -10,12 +10,16 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 </head>
 <body>
-<div class="container">
-    <div class="jumbotron">
-        <h1>@yield('cabecalho')</h1>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-2">
+        <a class="navbar-brand" href="#">Home</a>
+    </nav>
 
-    @yield('conteudo')
-</div>
+    <div class="container">
+        <div class="jumbotron">
+            <h1>@yield('cabecalho')</h1>
+        </div>
+
+        @yield('conteudo')
+    </div>
 </body>
 </html>
