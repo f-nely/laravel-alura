5df55b24095a051a398e7622f5774dc9702ae307
nely de faria <nelly.nf54@gmail.com>
2021-03-21 11:46:34 -0300

    Definindo o atributo valor para o input

--- controle-series/resources/views/episodios/index.blade.php
+++ controle-series/resources/views/episodios/index.blade.php
@@ -10,7 +10,7 @@
             @foreach($episodios as $episodio)
                 <li class="list-group-item d-flex justify-content-between align-items-center">
                     Episódio {{ $episodio->numero }}
-                    <input type="checkbox" name="episodios[]">
+                    <input type="checkbox" name="episodios[]" value="{{ $episodio->id }}">
                 </li>
             @endforeach
         </ul>
