7d2b2d961b5cab4b1f24a82bbd19aca1cbdbf2d1
nely de faria <nelly.nf54@gmail.com>
2021-03-21 12:38:38 -0300

    Cria método assistir

--- controle-series/app/Http/Controllers/EpisodiosController.php
+++ controle-series/app/Http/Controllers/EpisodiosController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Episodio;
 use App\Temporada;
 use Illuminate\Http\Request;
 
@@ -10,8 +11,20 @@ class EpisodiosController extends Controller
     public function index(Temporada $temporada)
     {
         return view('episodios.index', [
-            'episodios' => $temporada->episodios(),
+            'episodios' => $temporada->episodios,
             'temporadaId' => $temporada->id
         ]);
+    }
+
+    public function assistir(Temporada $temporada, Request $request)
+    {
+        $episodiosAssistido = $request->episodios;
+        $temporada->episodios->each(function (Episodio $episodio) use ($episodiosAssistido){
+            $episodio->assistido = in_array(
+                $episodio->id,
+                $episodiosAssistido
+            );
+        });
+        $temporada->push();
     }
 }
