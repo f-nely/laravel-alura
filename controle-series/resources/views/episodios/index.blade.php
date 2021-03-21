f70d7dc3190e21a1711f9acf9237084252b37ad8
nely de faria <nelly.nf54@gmail.com>
2021-03-21 11:54:16 -0300

    Definindo url do formulário

--- controle-series/resources/views/episodios/index.blade.php
+++ controle-series/resources/views/episodios/index.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('conteudo')
-    <form action="">
+    <form action="/temporada/{{ $temporadaId }}/episodios/assistir">
         <ul class="list-group">
             @foreach($episodios as $episodio)
                 <li class="list-group-item d-flex justify-content-between align-items-center">
