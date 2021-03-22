0641c365f6671b339912e9277e52bd2f7b13492d
nely de faria <nelly.nf54@gmail.com>
2021-03-22 11:54:40 -0300

    Renomeia método e cria seu escopo

--- controle-series/tests/Unit/TemporadaTest.php
+++ controle-series/tests/Unit/TemporadaTest.php
@@ -2,19 +2,33 @@
 
 namespace Tests\Unit;
 
+use App\Episodio;
+use App\Temporada;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TemporadaTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    public function testBuscaPorEpisodiosAssistidos()
     {
-        $this->assertTrue(true);
+        $temporada = new Temporada();
+        $episodio1 = new Episodio();
+        $episodio1->assistido = true;
+        $episodio2 = new Episodio();
+        $episodio2->assistido = false;
+        $episodio3 = new Episodio();
+        $episodio3->assistido = true;
+        $temporada->episodios->add($episodio1);
+        $temporada->episodios->add($episodio2);
+        $temporada->episodios->add($episodio3);
+
+        $episodiosAssistidos = $temporada->getEpisodiosAssistidos();
+
+        $this->assertCount(2, $episodiosAssistidos);
+        foreach ($episodiosAssistidos as $episodio) {
+            $this->assertTrue($episodio->assistido);
+        }
+
     }
 }
