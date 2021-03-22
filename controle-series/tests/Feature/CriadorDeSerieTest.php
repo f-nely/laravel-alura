a8a61360efd4257ce97ce8ac8cc74a4521f334ba
nely de faria <nelly.nf54@gmail.com>
2021-03-22 12:34:34 -0300

    Cria método testCriarSerie

--- controle-series/tests/Feature/CriadorDeSerieTest.php
+++ controle-series/tests/Feature/CriadorDeSerieTest.php
@@ -2,9 +2,21 @@
 
 namespace Tests\Feature;
 
+use App\Serie;
+use App\Services\CriadorDeSerie;
 use Tests\TestCase;
 
 class CriadorDeSerieTest extends TestCase
 {
+    public function testCriarSerie()
+    {
+        $criadorDeSerie = new CriadorDeSerie();
+        $nomeSerie = 'Nome de Teste';
+        $serieCriada = $criadorDeSerie->criarSerie($nomeSerie, 1, 1);
 
+        $this->assertInstanceOf(Serie::class, $serieCriada);
+        $this->assertDatabaseHas('series', ['nome' => $nomeSerie]);
+        $this->assertDatabaseHas('temporadas', ['serie_id' => $serieCriada->id, 'numero' => 1]);
+        $this->assertDatabaseHas('episodios', ['numero' => 1]);
+    }
 }
