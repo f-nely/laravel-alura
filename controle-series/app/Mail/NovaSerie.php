ab7fd07e3647457de09fc2cf4b10b51a8362abc9
nely de faria <nelly.nf54@gmail.com>
2021-03-23 14:08:13 -0300

    Definindo novos atributos

--- controle-series/app/Mail/NovaSerie.php
+++ controle-series/app/Mail/NovaSerie.php
@@ -2,6 +2,8 @@
 
 namespace App\Mail;
 
+use App\Episodio;
+use App\Temporada;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
@@ -15,16 +17,26 @@ class NovaSerie extends Mailable
      * @var string
      */
     public $nome;
+    /**
+     * @var Temporada
+     */
+    public $qtdTemporada;
+    /**
+     * @var Episodio
+     */
+    public $qtdEpisodios;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct(string $nome)
+    public function __construct(string $nome, Temporada $qtdTemporada, Episodio $qtdEpisodios)
     {
         //
         $this->nome = $nome;
+        $this->qtdTemporada = $qtdTemporada;
+        $this->qtdEpisodios = $qtdEpisodios;
     }
 
     /**
