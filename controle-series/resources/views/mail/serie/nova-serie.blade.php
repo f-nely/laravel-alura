372179ff4c16fc9f51f264f3496ef5586899684a
nely de faria <nelly.nf54@gmail.com>
2021-03-23 14:13:31 -0300

    Adicionando quebra de linha

--- controle-series/resources/views/mail/serie/nova-serie.blade.php
+++ controle-series/resources/views/mail/serie/nova-serie.blade.php
@@ -1,8 +1,8 @@
 <html>
 <body>
     <h1>Nova Série</h1>
-    Nome da Série: {{ $nome }}
-    Qtd Temporadas: {{ $qtdTemporadas }}
-    Qtd Episodios: {{ $qtdEpisodios }}
+    Nome da Série: {{ $nome }} <br>
+    Qtd Temporadas: {{ $qtdTemporadas }} <br>
+    Qtd Episodios: {{ $qtdEpisodios }} <br>
 </body>
 </html>
