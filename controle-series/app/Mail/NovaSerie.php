e0f64e360ec1b6911cdc370f0eb1de54f5142c60
nely de faria <nelly.nf54@gmail.com>
2021-03-23 13:46:36 -0300

    Alterando o método build

--- controle-series/app/Mail/NovaSerie.php
+++ controle-series/app/Mail/NovaSerie.php
@@ -28,6 +28,6 @@ class NovaSerie extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->view('mail.serie.nova-serie');
     }
 }
