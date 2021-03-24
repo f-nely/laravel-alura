c171ece691d03462152d59773e35a77d642bd3ee
nely de faria <nelly.nf54@gmail.com>
2021-03-24 12:59:38 -0300

    Programando método handle

--- controle-series/app/Listeners/EnviarEmailNovaSerieCadastrada.php
+++ controle-series/app/Listeners/EnviarEmailNovaSerieCadastrada.php
@@ -3,8 +3,10 @@
 namespace App\Listeners;
 
 use App\Events\NovaSerie;
+use App\User;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Mail;
 
 class EnviarEmailNovaSerieCadastrada
 {
@@ -26,6 +28,23 @@ class EnviarEmailNovaSerieCadastrada
      */
     public function handle(NovaSerie $event)
     {
-        //
+        $nomeSerie = $event->nomeSerie;
+        $qtdTemporadas = $event->qtdTemporadas;
+        $qtdEpisodios = $event->qtdEpisodios;
+        $users = User::all();
+        foreach ($users as $index => $user) {
+            $multiplicador = $index + 1;
+            $email = new \App\Mail\NovaSerie(
+                $nomeSerie,
+                $qtdTemporadas,
+                $qtdEpisodios
+            );
+            $email->subject = 'Nova Série Adicionada';
+            $when = now()->addSecond($multiplicador * 5);
+            Mail::to($user)->later(
+                $when,
+                $email
+            );
+        }
     }
 }
