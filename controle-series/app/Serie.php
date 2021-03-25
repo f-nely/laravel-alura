cf62b6f275588352f8d78416b117c99cd9bb8a6e
nely de faria <nelly.nf54@gmail.com>
2021-03-25 20:28:21 -0300

    Cria método getCapaUrlAttribute

--- controle-series/app/Serie.php
+++ controle-series/app/Serie.php
@@ -11,6 +11,16 @@ class Serie extends Model
     public $timestamps = false;
     protected $fillable = ['nome', 'capa'];
 
+    public function getCapaUrlAttribute()
+    {
+        if ($this->capa) {
+            return 'http://localhost:8000/storage/' . $this->capa;
+        }
+
+        return 'http://localhost:8000/storage/serie/sem-imagem.jpg';
+
+    }
+
     public function temporadas()
     {
         return $this->hasMany(Temporada::class);
