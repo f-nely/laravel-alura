8b5629f7bf7ef6881f53f6139814b091fc8d7a29
nely de faria <nelly.nf54@gmail.com>
2021-03-25 19:08:00 -0300

    Adicionando a propriedade capa

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -33,7 +33,8 @@ class SeriesController extends Controller
         $serie = $criadorDeSerie->criarSerie(
             $request->nome,
             $request->qtd_temporadas,
-            $request->ep_por_temporada
+            $request->ep_por_temporada,
+            $request->capa
         );
 
         $eventoNovaSerie = new NovaSerie(
