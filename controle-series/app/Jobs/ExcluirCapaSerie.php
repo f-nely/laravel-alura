d65666600aab7ec33f515469640f10eeaf9734b6
nely de faria <nelly.nf54@gmail.com>
2021-03-26 13:36:17 -0300

    Inicializa propriedade serie e programando método handle

--- controle-series/app/Jobs/ExcluirCapaSerie.php
+++ controle-series/app/Jobs/ExcluirCapaSerie.php
@@ -7,19 +7,22 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Storage;
 
-class ExcluirCapaSerie implements ShouldQueue
+class ExcluirCapaSerie //implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $serie;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($serie)
     {
-        //
+        $this->serie = $serie;
     }
 
     /**
@@ -29,6 +32,10 @@ class ExcluirCapaSerie implements ShouldQueue
      */
     public function handle()
     {
-        //
+        $serie = $this->serie;
+
+        if ($serie->capa) {
+            Storage::delete($serie->capa);
+        }
     }
 }
